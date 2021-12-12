9fa9f8a3258030182b8f455f2398fe3690be7f9c
AmrDeveloper <amr96@programmer.net>
2021-12-12 10:23:28 +0200

    Add ArrayAdapter in supported annotations | create Parser helper class | Improve code style

--- easyadapter-compiler/src/main/java/com/amrdeveloper/easyadapter/compiler/EasyAdapterProcessor.kt
+++ easyadapter-compiler/src/main/java/com/amrdeveloper/easyadapter/compiler/EasyAdapterProcessor.kt
@@ -1,237 +1,78 @@
 package com.amrdeveloper.easyadapter.compiler
 
 import com.amrdeveloper.easyadapter.adapter.*
-import com.amrdeveloper.easyadapter.bind.*
 import com.amrdeveloper.easyadapter.compiler.generator.*
-import com.amrdeveloper.easyadapter.compiler.model.ListAdapterData
-import com.amrdeveloper.easyadapter.compiler.model.*
+import com.amrdeveloper.easyadapter.compiler.parser.AdapterParser
 import com.amrdeveloper.easyadapter.compiler.utils.EasyAdapterLogger
 import com.squareup.kotlinpoet.FileSpec
-import java.io.File
 import javax.annotation.processing.AbstractProcessor
 import javax.annotation.processing.ProcessingEnvironment
 import javax.annotation.processing.RoundEnvironment
 import javax.lang.model.SourceVersion
-import javax.lang.model.element.Element
 import javax.lang.model.element.TypeElement
+import javax.lang.model.util.Elements
 
 class EasyAdapterProcessor : AbstractProcessor() {
 
     private lateinit var logger: EasyAdapterLogger
+    private lateinit var elementUtils : Elements
 
     override fun init(processingEnv: ProcessingEnvironment?) {
         super.init(processingEnv)
         processingEnv?.let {
             logger = EasyAdapterLogger(it)
+            elementUtils = it.elementUtils
         }
     }
 
     override fun process(annotations: MutableSet<out TypeElement>, environment: RoundEnvironment): Boolean {
-        val kaptGeneratedDir = processingEnv.options[KAPT_KOTLIN_GENERATED_OPTION_NAME] ?: return false
+        val generatedDirectory = processingEnv.filer
 
-        val recyclerAdapterElements = environment.getElementsAnnotatedWith(RecyclerAdapter::class.java)
-        for (adapterElement in recyclerAdapterElements) {
-            val recyclerAdapter = parseRecyclerAdapterClass(adapterElement)
-            FileSpec.builder(recyclerAdapter.adapterPackageName, recyclerAdapter.adapterClassName)
-                .addType(RecyclerAdapterGenerator(recyclerAdapter).generate())
-                .build()
-                .writeTo(File(kaptGeneratedDir))
+        environment.getElementsAnnotatedWith(RecyclerAdapter::class.java).forEach {
+            val adapter = AdapterParser.parseRecyclerAdapter(elementUtils, it)
+            FileSpec.builder(adapter.adapterPackageName, adapter.adapterClassName)
+                .addType(RecyclerAdapterGenerator(adapter).generate()).build()
+                .writeTo(generatedDirectory)
         }
 
-        val listAdapterElements = environment.getElementsAnnotatedWith(ListAdapter::class.java)
-        for (adapterElement in listAdapterElements) {
-            val listAdapter = parseListAdapterClass(adapterElement)
-            FileSpec.builder(listAdapter.adapterPackageName, listAdapter.adapterClassName)
-                .addType(ListAdapterGenerator(listAdapter).generate())
-                .build()
-                .writeTo(File(kaptGeneratedDir))
+        environment.getElementsAnnotatedWith(ListAdapter::class.java).forEach {
+            val adapter = AdapterParser.parseListAdapter(elementUtils, it)
+            FileSpec.builder(adapter.adapterPackageName, adapter.adapterClassName)
+                .addType(ListAdapterGenerator(adapter).generate()).build()
+                .writeTo(generatedDirectory)
         }
 
-        val pagingAdapterElements = environment.getElementsAnnotatedWith(PagingDataAdapter::class.java)
-        for (adapterElement in pagingAdapterElements) {
-            val pagingAdapter = parsePagingDataAdapter(adapterElement)
+        environment.getElementsAnnotatedWith(PagingDataAdapter::class.java).forEach {
+            val pagingAdapter = AdapterParser.parsePagingDataAdapter(elementUtils, it)
             FileSpec.builder(pagingAdapter.adapterPackageName, pagingAdapter.adapterClassName)
-                .addType(PagingDataAdapterGenerator(pagingAdapter).generate())
-                .build()
-                .writeTo(File(kaptGeneratedDir))
+                .addType(PagingDataAdapterGenerator(pagingAdapter).generate()).build()
+                .writeTo(generatedDirectory)
         }
 
-        val pagedListAdapterElements = environment.getElementsAnnotatedWith(PagedListAdapter::class.java)
-        for (adapterElement in pagedListAdapterElements) {
-            val pagedAdapter = parsePagedListAdapter(adapterElement)
+        environment.getElementsAnnotatedWith(PagedListAdapter::class.java).forEach {
+            val pagedAdapter = AdapterParser.parsePagedListAdapter(elementUtils, it)
             FileSpec.builder(pagedAdapter.adapterPackageName, pagedAdapter.adapterClassName)
-                .addType(PagedListAdapterGenerator(pagedAdapter).generate())
-                .build()
-                .writeTo(File(kaptGeneratedDir))
+                .addType(PagedListAdapterGenerator(pagedAdapter).generate()).build()
+                .writeTo(generatedDirectory)
         }
 
-        val arrayAdapterElements = environment.getElementsAnnotatedWith(ArrayAdapter::class.java)
-        for (adapterElement in arrayAdapterElements) {
-            val arrayAdapter = parseArrayAdapter(adapterElement)
+        environment.getElementsAnnotatedWith(ArrayAdapter::class.java).forEach {
+            val arrayAdapter = AdapterParser.parseArrayAdapter(elementUtils, it)
             FileSpec.builder(arrayAdapter.adapterPackageName, arrayAdapter.adapterClassName)
-                .addType(ArrayAdapterGenerator(arrayAdapter).generate())
-                .build()
-                .writeTo(File(kaptGeneratedDir))
+                .addType(ArrayAdapterGenerator(arrayAdapter).generate()).build()
+                .writeTo(generatedDirectory)
         }
 
         return true
     }
 
-    private fun parseRecyclerAdapterClass(element: Element) : RecyclerAdapterData {
-        val className = element.simpleName.toString()
-        val adapterPackageName = processingEnv.elementUtils.getPackageOf(element).toString()
-        val annotation = element.getAnnotation(RecyclerAdapter::class.java)
-        val appPackageName = annotation.appPackageName
-        val layoutId = annotation.layoutId
-        val generateUpdateData = annotation.generateUpdateData
-        val adapterClassName = if (annotation.customClassName.isEmpty()) "${className}RecyclerAdapter" else annotation.customClassName
-        val viewBindingDataList = parseAdapterBindingList(element.enclosedElements)
-
-        return RecyclerAdapterData (
-            appPackageName,
-            adapterPackageName,
-            adapterClassName,
-            className,
-            layoutId,
-            viewBindingDataList,
-            generateUpdateData,
-        )
-    }
-
-    private fun parseListAdapterClass(element: Element) : ListAdapterData {
-        val className = element.simpleName.toString()
-        val adapterPackageName = processingEnv.elementUtils.getPackageOf(element).toString()
-        val annotation = element.getAnnotation(ListAdapter::class.java)
-        val appPackageName = annotation.appPackageName
-        val layoutId = annotation.layoutId
-        val diffUtilContent = annotation.diffUtilContent
-        val adapterClassName = if (annotation.customClassName.isEmpty()) "${className}ListAdapter" else annotation.customClassName
-        val viewBindingDataList = parseAdapterBindingList(element.enclosedElements)
-
-        return ListAdapterData (
-            appPackageName,
-            adapterPackageName,
-            adapterClassName,
-            className,
-            layoutId,
-            viewBindingDataList,
-            diffUtilContent,
-        )
-    }
-
-    private fun parsePagingDataAdapter(element: Element) : PagingAdapterData {
-        val className = element.simpleName.toString()
-        val adapterPackageName = processingEnv.elementUtils.getPackageOf(element).toString()
-        val annotation = element.getAnnotation(PagingDataAdapter::class.java)
-        val appPackageName = annotation.appPackageName
-        val layoutId = annotation.layoutId
-        val diffUtilContent = annotation.diffUtilContent
-        val adapterClassName = if (annotation.customClassName.isEmpty()) "${className}PagingDataAdapter" else annotation.customClassName
-        val viewBindingDataList = parseAdapterBindingList(element.enclosedElements)
-
-        return PagingAdapterData (
-            appPackageName,
-            adapterPackageName,
-            adapterClassName,
-            className,
-            layoutId,
-            viewBindingDataList,
-            diffUtilContent,
-        )
-    }
-
-    private fun parsePagedListAdapter(element: Element) : PagedListAdapterData {
-        val className = element.simpleName.toString()
-        val adapterPackageName = processingEnv.elementUtils.getPackageOf(element).toString()
-        val annotation = element.getAnnotation(PagedListAdapter::class.java)
-        val appPackageName = annotation.appPackageName
-        val layoutId = annotation.layoutId
-        val diffUtilContent = annotation.diffUtilContent
-        val adapterClassName = if (annotation.customClassName.isEmpty()) "${className}PagedListAdapter" else annotation.customClassName
-        val viewBindingDataList = parseAdapterBindingList(element.enclosedElements)
-
-        return PagedListAdapterData (
-            appPackageName,
-            adapterPackageName,
-            adapterClassName,
-            className,
-            layoutId,
-            viewBindingDataList,
-            diffUtilContent,
-        )
-    }
-
-    private fun parseAdapterBindingList(enclosedElements : List<Element>) : List<BindingData> {
-        val viewBindingDataList = mutableListOf<BindingData>()
-        for (element in enclosedElements) {
-            val textViewBinding = element.getAnnotation(BindText::class.java)
-            if (textViewBinding != null) {
-                val binding = BindingTextData(textViewBinding.value, textViewBinding.viewId)
-                viewBindingDataList.add(binding)
-            }
-
-            val imageResBinding = element.getAnnotation(BindImageRes::class.java)
-            if (imageResBinding != null) {
-                val binding = BindImageResData(imageResBinding.value, imageResBinding.viewId)
-                viewBindingDataList.add(binding)
-            }
-
-            val backgroundResBinding = element.getAnnotation(BindBackgroundRes::class.java)
-            if (backgroundResBinding != null) {
-                val binding = BindBackgroundResData(backgroundResBinding.value, backgroundResBinding.viewId)
-                viewBindingDataList.add(binding)
-            }
-
-            val backgroundColorBinding = element.getAnnotation(BindBackgroundColor::class.java)
-            if (backgroundColorBinding != null) {
-                val binding = BindBackgroundColorData(backgroundColorBinding.value, backgroundColorBinding.viewId)
-                viewBindingDataList.add(binding)
-            }
-
-            val visibilityBinding = element.getAnnotation(BindVisibility::class.java)
-            if (visibilityBinding != null) {
-                val binding = BindVisibilityData(visibilityBinding.value, visibilityBinding.viewId)
-                viewBindingDataList.add(binding)
-            }
-
-            val imageBinding = element.getAnnotation(BindImage::class.java)
-            if (imageBinding != null) {
-                val binding = BindImageData(imageBinding.value, imageBinding.viewId, imageBinding.loader)
-                viewBindingDataList.add(binding)
-            }
-        }
-        return viewBindingDataList
-    }
-
-    private fun parseArrayAdapter(element: Element) : ArrayAdapterData {
-        val className = element.simpleName.toString()
-        val adapterPackageName = processingEnv.elementUtils.getPackageOf(element).toString()
-        val annotation = element.getAnnotation(ArrayAdapter::class.java)
-        val appPackageName = annotation.appPackageName
-        val layoutId = annotation.layoutId
-        val adapterClassName = if (annotation.customClassName.isEmpty()) "${className}ArrayAdapter" else annotation.customClassName
-        val viewBindingDataList = parseAdapterBindingList(element.enclosedElements)
-
-        return ArrayAdapterData (
-            appPackageName,
-            adapterPackageName,
-            adapterClassName,
-            className,
-            layoutId,
-            viewBindingDataList,
-        )
-    }
-
-    override fun getSupportedAnnotationTypes() = mutableSetOf<String> (
+    override fun getSupportedAnnotationTypes() = mutableSetOf (
         RecyclerAdapter::class.java.canonicalName,
         ListAdapter::class.java.canonicalName,
         PagingDataAdapter::class.java.canonicalName,
-        PagedListAdapter::class.java.canonicalName
+        PagedListAdapter::class.java.canonicalName,
+        ArrayAdapter::class.java.canonicalName
     )
 
     override fun getSupportedSourceVersion(): SourceVersion = SourceVersion.latestSupported()
-
-    companion object {
-        const val KAPT_KOTLIN_GENERATED_OPTION_NAME = "kapt.kotlin.generated"
-    }
 }
---
+++ easyadapter-compiler/src/main/java/com/amrdeveloper/easyadapter/compiler/parser/AdapterParser.kt
@@ -0,0 +1,150 @@
+package com.amrdeveloper.easyadapter.compiler.parser
+
+import com.amrdeveloper.easyadapter.adapter.*
+import com.amrdeveloper.easyadapter.bind.*
+import com.amrdeveloper.easyadapter.compiler.model.*
+import javax.lang.model.element.Element
+import javax.lang.model.util.Elements
+
+object AdapterParser {
+
+    fun parseRecyclerAdapter(elementUtils : Elements, element: Element) : RecyclerAdapterData {
+        val className = element.simpleName.toString()
+        val adapterPackageName = elementUtils.getPackageOf(element).toString()
+        val annotation = element.getAnnotation(RecyclerAdapter::class.java)
+        val appPackageName = annotation.appPackageName
+        val layoutId = annotation.layoutId
+        val generateUpdateData = annotation.generateUpdateData
+        val adapterClassName = if (annotation.customClassName.isEmpty()) "${className}RecyclerAdapter" else annotation.customClassName
+        val viewBindingDataList = parseAdapterBindingList(element.enclosedElements)
+
+        return RecyclerAdapterData (
+            appPackageName,
+            adapterPackageName,
+            adapterClassName,
+            className,
+            layoutId,
+            viewBindingDataList,
+            generateUpdateData,
+        )
+    }
+
+    fun parseListAdapter(elementUtils : Elements, element: Element) : ListAdapterData {
+        val className = element.simpleName.toString()
+        val adapterPackageName = elementUtils.getPackageOf(element).toString()
+        val annotation = element.getAnnotation(ListAdapter::class.java)
+        val appPackageName = annotation.appPackageName
+        val layoutId = annotation.layoutId
+        val diffUtilContent = annotation.diffUtilContent
+        val adapterClassName = if (annotation.customClassName.isEmpty()) "${className}ListAdapter" else annotation.customClassName
+        val viewBindingDataList = parseAdapterBindingList(element.enclosedElements)
+
+        return ListAdapterData (
+            appPackageName,
+            adapterPackageName,
+            adapterClassName,
+            className,
+            layoutId,
+            viewBindingDataList,
+            diffUtilContent,
+        )
+    }
+
+    fun parsePagingDataAdapter(elementUtils : Elements, element: Element) : PagingAdapterData {
+        val className = element.simpleName.toString()
+        val adapterPackageName = elementUtils.getPackageOf(element).toString()
+        val annotation = element.getAnnotation(PagingDataAdapter::class.java)
+        val appPackageName = annotation.appPackageName
+        val layoutId = annotation.layoutId
+        val diffUtilContent = annotation.diffUtilContent
+        val adapterClassName = if (annotation.customClassName.isEmpty()) "${className}PagingDataAdapter" else annotation.customClassName
+        val viewBindingDataList = parseAdapterBindingList(element.enclosedElements)
+
+        return PagingAdapterData (
+            appPackageName,
+            adapterPackageName,
+            adapterClassName,
+            className,
+            layoutId,
+            viewBindingDataList,
+            diffUtilContent,
+        )
+    }
+
+    fun parsePagedListAdapter(elementUtils : Elements, element: Element) : PagedListAdapterData {
+        val className = element.simpleName.toString()
+        val adapterPackageName = elementUtils.getPackageOf(element).toString()
+        val annotation = element.getAnnotation(PagedListAdapter::class.java)
+        val appPackageName = annotation.appPackageName
+        val layoutId = annotation.layoutId
+        val diffUtilContent = annotation.diffUtilContent
+        val adapterClassName = if (annotation.customClassName.isEmpty()) "${className}PagedListAdapter" else annotation.customClassName
+        val viewBindingDataList = parseAdapterBindingList(element.enclosedElements)
+
+        return PagedListAdapterData (
+            appPackageName,
+            adapterPackageName,
+            adapterClassName,
+            className,
+            layoutId,
+            viewBindingDataList,
+            diffUtilContent,
+        )
+    }
+
+    fun parseArrayAdapter(elementUtils : Elements, element: Element) : ArrayAdapterData {
+        val className = element.simpleName.toString()
+        val adapterPackageName = elementUtils.getPackageOf(element).toString()
+        val annotation = element.getAnnotation(ArrayAdapter::class.java)
+        val appPackageName = annotation.appPackageName
+        val layoutId = annotation.layoutId
+        val adapterClassName = if (annotation.customClassName.isEmpty()) "${className}ArrayAdapter" else annotation.customClassName
+        val viewBindingDataList = parseAdapterBindingList(element.enclosedElements)
+
+        return ArrayAdapterData (
+            appPackageName,
+            adapterPackageName,
+            adapterClassName,
+            className,
+            layoutId,
+            viewBindingDataList,
+        )
+    }
+
+    private fun parseAdapterBindingList(enclosedElements : List<Element>) : List<BindingData> {
+        val viewBindingDataList = mutableListOf<BindingData>()
+        for (element in enclosedElements) {
+            element.getAnnotation(BindText::class.java)?.let {
+                val binding = BindingTextData(it.value, it.viewId)
+                viewBindingDataList.add(binding)
+            }
+
+            element.getAnnotation(BindImageRes::class.java)?.let {
+                val binding = BindImageResData(it.value, it.viewId)
+                viewBindingDataList.add(binding)
+            }
+
+            element.getAnnotation(BindBackgroundRes::class.java)?.let {
+                val binding = BindBackgroundResData(it.value, it.viewId)
+                viewBindingDataList.add(binding)
+            }
+
+            element.getAnnotation(BindBackgroundColor::class.java)?.let {
+                val binding = BindBackgroundColorData(it.value, it.viewId)
+                viewBindingDataList.add(binding)
+            }
+
+            element.getAnnotation(BindVisibility::class.java)?.let {
+                val binding = BindVisibilityData(it.value, it.viewId)
+                viewBindingDataList.add(binding)
+            }
+
+            element.getAnnotation(BindImage::class.java)?.let {
+                val binding = BindImageData(it.value, it.viewId, it.loader)
+                viewBindingDataList.add(binding)
+            }
+        }
+        return viewBindingDataList
+    }
+
+}
