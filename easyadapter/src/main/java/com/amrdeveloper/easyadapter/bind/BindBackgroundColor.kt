f412fa2eede2372a75bbd6afbdc38577b8f497ff
AmrDeveloper <amr96@programmer.net>
2022-01-19 13:30:20 +0200

    Write a full javadoc for all Bind Annotations

--- easyadapter/src/main/java/com/amrdeveloper/easyadapter/bind/BindBackgroundColor.kt
+++ easyadapter/src/main/java/com/amrdeveloper/easyadapter/bind/BindBackgroundColor.kt
@@ -1,7 +1,37 @@
+/*
+* MIT License
+*
+* Copyright (c) 2021 AmrDeveloper (Amr Hesham)
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
 package com.amrdeveloper.easyadapter.bind
 
+/**
+ * Mark model class integer field to used it as a background color
+ */
 @Target(AnnotationTarget.FIELD)
 @Retention(AnnotationRetention.SOURCE)
 annotation class BindBackgroundColor (
+    /**
+     * The view id without R.id to apply the bind feature on it
+     */
     val viewId : String,
 )
